ac387bbd30e9b4918e5f028b677937c247725126
dongmao zhang <deanraccoon@gmail.com>
2012-11-20 17:34:15 +0800

    add ub.sh

---
+++ ub.sh
@@ -0,0 +1,18 @@
+make start
+sleep 5
+echo "lease acquire"|netcat localhost 1235
+sleep 3
+echo "lease list"|netcat localhost 1236
+echo "lease list"|netcat localhost 1235
+echo "lease list"|netcat localhost 1237
+sleep 2
+echo "kill run.py 1"
+pid=$(pgrep -f "run.py 1")
+kill $pid
+sleep 3
+
+python run.py 1 &
+sleep 3
+
+echo "lease slowlist"|netcat localhost 1235
+echo "lease list"|netcat localhost 1235
